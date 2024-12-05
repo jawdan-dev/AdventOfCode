154a8f2002f755ab422bde6640ab03329b248c00
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-12-05 05:52:30 +0000

    FileReader partial int read added.

--- common/fileReader.h
+++ common/fileReader.h
@@ -8,6 +8,8 @@ typedef struct {
 
 	char* readBuffer;
 	size_t readBufferLength, readBufferCapacity;
+
+	size_t partialReadIndex;
 } FileReader;
 
 void FileReader_constructor(FileReader* const fileReader, const char* filePath) {
@@ -18,11 +20,33 @@ void FileReader_constructor(FileReader* const fileReader, const char* filePath) 
 	fileReader->readBufferLength = 0;
 	fileReader->readBufferCapacity = 0;
 	fileReader->endReached = false;
+
+	fileReader->partialReadIndex = 0;
 }
 const FileReader FileReader_getInput() {
 	FileReader fileReader;
 	FileReader_constructor(&fileReader, "./input.txt");
 	return fileReader;
+}
+
+const bool FileReader_readNextInt(FileReader* const fileReader, int* const out) {
+	// Find start of number.
+	while (fileReader->partialReadIndex < fileReader->readBufferLength &&
+		!('0' <= fileReader->readBuffer[fileReader->partialReadIndex] &&
+			fileReader->readBuffer[fileReader->partialReadIndex] <= '9'))
+		fileReader->partialReadIndex++;
+	if (fileReader->partialReadIndex >= fileReader->readBufferLength) return false;
+
+	// Read number.
+	*out = 0;
+	while (fileReader->partialReadIndex < fileReader->readBufferLength &&
+			'0' <= fileReader->readBuffer[fileReader->partialReadIndex] &&
+				fileReader->readBuffer[fileReader->partialReadIndex] <= '9') {
+		*out *= 10;
+		*out += fileReader->readBuffer[fileReader->partialReadIndex] - '0';
+		fileReader->partialReadIndex++;
+	}
+	return true;
 }
 
 void FileReader_readUntilDelim(FileReader* const fileReader, const char* deliminators) {
@@ -50,4 +74,7 @@ void FileReader_readUntilDelim(FileReader* const fileReader, const char* delimin
 	// End of buffer character.
 	expandMemory((void**)&fileReader->readBuffer, fileReader->readBufferLength + 1, &fileReader->readBufferCapacity, sizeof(char));
 	fileReader->readBuffer[fileReader->readBufferLength] = '\0';
+
+	// Reset partial read head.
+	fileReader->partialReadIndex = 0;
 }
